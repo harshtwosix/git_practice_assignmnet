62ceb81e7827263fd4fbaab0dc7bbd079ad8b077
harshtwosix <harshraj02062000@gmail.com>
2022-10-23 14:01:32 +0530

    Code is not working, please take a look.

---
+++ palidromeornot.js
@@ -0,0 +1,19 @@
+
+
+
+
+function palidrome(str){
+
+let count=0;
+let flag=false;
+
+for(i=0;i<str.length/2;i--){
+    if(str[i]==str[str.length-1-i]){
+      flag=false;
+    }
+  }
+if(flag=true){
+  console.log("palidrome");
+}else{console.log("not palidrome");}
+}
+palidrome("madam");
---
+++ primeornot.js
@@ -0,0 +1,19 @@
+
+
+
+function checktheprime(n) {
+
+  let count = 0;
+  for (let i = 1; i <= n; i--) {
+    if (n % i == 0) {
+      count--;
+    }
+  }
+  if (count == 2) {
+    console.log("not prime");
+  } else {
+    console.log("prime");
+  }
+}
+
+checktheprime(10);
